81a9529a7f3600d30277a638c510ff0f39aeefe8
gaoyaqiu <30494613@qq.com>
2018-12-29 16:42:25 +0800

    更新nginx路径

--- utils/abtesting.sh
+++ utils/abtesting.sh
@@ -2,9 +2,9 @@
 
 rm *temp -rf
 
-/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/nginx.conf  
-/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/stable.conf 
-/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/beta1.conf  
-/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/beta2.conf  
-/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/beta3.conf  
-/usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/beta4.conf  
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/nginx.conf  
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/stable.conf 
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/beta1.conf  
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/beta2.conf  
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/beta3.conf  
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/beta4.conf  
--- utils/stop-all-nginx.sh
+++ utils/stop-all-nginx.sh
@@ -1,8 +1,8 @@
 rm *temp -rf
 
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/nginx.conf  -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/stable.conf -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta1.conf  -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta2.conf  -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta3.conf  -s stop
-/usr/local/nginx/sbin/nginx -p `pwd` -c conf/beta4.conf  -s stop
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/nginx.conf  -s stop
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/stable.conf -s stop
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/beta1.conf  -s stop
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/beta2.conf  -s stop
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/beta3.conf  -s stop
+/usr/local/Cellar/openresty/1.13.6.1/nginx/sbin/nginx  -p `pwd` -c conf/beta4.conf  -s stop
